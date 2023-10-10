bbe829343a80fc74baa27744ff073679fc8be0fe
JESUSMADA <147555237+JESUSMADA@users.noreply.github.com>
2023-10-10 14:56:08 -0700

    Add files via upload

---
+++ calcularArea/sripts.js
@@ -0,0 +1,26 @@
+//boton
+function calcularArea() {
+    
+    //seleccionar figura
+    const figura = document.getElementById("figura").value;
+    //resultado
+    const resultado = document.getElementById("resultado");
+    //altura y base
+    const altura = parseFloat(document.getElementById("altura").value);
+    const base = parseFloat(document.getElementById("base").value);
+
+    let area;
+
+    //calcular
+    if (figura == "cuadro") {
+        area = altura * altura;
+    } else if (figura == "rectangulo") {
+        area = altura * base;
+    } else if (figura == "triangulo") {
+        area = (altura * base) / 2;
+    }
+
+    //resultado
+    resultado.textContent = `El area es: ${area}`;
+    resultado.innerHtml = `El area es: ${area}`;
+}
